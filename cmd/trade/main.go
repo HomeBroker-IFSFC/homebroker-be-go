bda65bc9a488379edd3da5116ae4b0fce9183016
JoaoRafa19 <joaopedrorafael19@gmail.com>
2023-07-23 13:00:55 -0300

    Kafka integration

--- cmd/trade/main.go
+++ cmd/trade/main.go
@@ -5,7 +5,7 @@ import (
 	"fmt"
 	"sync"
 
-	"github.com/JoaoRafa19/homebroker/go/internal/infra/kafka"
+	"github.com/JoaoRafa19/homebroker/go/internal/infra/kafkaclient"
 	"github.com/JoaoRafa19/homebroker/go/internal/market/dto"
 	"github.com/JoaoRafa19/homebroker/go/internal/market/entity"
 	"github.com/JoaoRafa19/homebroker/go/internal/market/transformer"
@@ -13,6 +13,7 @@ import (
 )
 
 func main() {
+	fmt.Println("Inicio programa")
 	ordersIn := make(chan *entity.Order)
 	ordersOut := make(chan *entity.Order)
 	wg := &sync.WaitGroup{}
@@ -21,13 +22,14 @@ func main() {
 	kafkaMsgChannel := make(chan *ckafka.Message)
 
 	configMap := &ckafka.ConfigMap{
-		"bootstrap.servers":"host.docker.internal:9094", 
-		"group.id": "myGroup",
-		"auto.offset.reset":"earliest",
+		"bootstrap.servers": "host.docker.internal:9094",
+		"group.id":          "myGroup",
+		"auto.offset.reset": "latest",
 	}
+	fmt.Println(configMap)
 
-	producer := kafka.NewKafkaProducer(configMap)
-	kafka := kafka.NewConsumer(configMap, []string{"input"})
+	producer := kafkaclient.NewKafkaProducer(configMap)
+	kafka := kafkaclient.NewConsumer(configMap, []string{"input"})
 
 	go kafka.Consume(kafkaMsgChannel) // Thread 2
 
@@ -35,24 +37,26 @@ func main() {
 	book := entity.NewBook(ordersIn, ordersOut, wg)
 	go book.Trade() // Thread 3
 
-	go func () {
+	go func() {
+		fmt.Println("Iniciou o listen de messages...")
 		for msg := range kafkaMsgChannel {
 			wg.Add(1)
-			fmt.Println(string(msg.Value))
-			tradeInput := dto.TradeInput {}
-			err := json.Unmarshal(msg.Value, &tradeInput)
-			if err == nil {
-				panic(err)
+			if msg != nil {
+				fmt.Println(string(msg.Value))
+				tradeInput := dto.TradeInput{}
+				err := json.Unmarshal(msg.Value, &tradeInput)
+				if err != nil {
+					panic(err)
+				}
+				order := transformer.TransformInput(tradeInput)
+				ordersIn <- order
 			}
-			order:= transformer.TransformInput(tradeInput)
-			ordersIn <- order
 		}
-	} ()
-
+	}()
 
 	for res := range ordersOut {
 		output := transformer.TransformOutput(res)
-		outputJson,err := json.MarshalIndent( output, "", "    ")
+		outputJson, err := json.MarshalIndent(output, "", "    ")
 		if err != nil {
 			fmt.Println(err)
 		}
---
+++ internal/infra/kafkaclient/consumer.go
@@ -0,0 +1,35 @@
+package kafkaclient
+
+import (
+	"github.com/confluentinc/confluent-kafka-go/kafka"
+)
+
+type Consumer struct {
+	ConfigMap *kafka.ConfigMap
+	Topics    []string
+}
+
+func NewConsumer(configMap *kafka.ConfigMap, topics []string) *Consumer {
+	return &Consumer{
+		ConfigMap: configMap,
+		Topics:    topics,
+	}
+}
+
+func (c *Consumer) Consume(msgChan chan *kafka.Message) {
+	consumer, err := kafka.NewConsumer(c.ConfigMap)
+	if err != nil {
+		panic(err)
+	}
+	err = consumer.SubscribeTopics(c.Topics, nil)
+	if err != nil {
+		panic(err)
+	}
+	for {
+		msg, err := consumer.ReadMessage(-1)
+		if err != nil {
+			msgChan <- msg
+		}
+	}
+
+}
